684c200680479d445d97574d3c11d83a5a22cdd4
Sabal11 <labasking4@gmail.com>
2024-03-04 14:46:18 +0545

    library for basic math in python

---
+++ math_functions.py
@@ -0,0 +1,9 @@
+import math
+x = 3.6
+y = -2
+a = 10
+print (round(x)) # rounds up decimal number.
+print (abs(y)) # always gives positive number.
+print (math.floor(x))
+print (math.gcd(a , y)) #math function used
+print (math.pi)
